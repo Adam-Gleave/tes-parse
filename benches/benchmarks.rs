37686adb79f9445d568b20b809d5b7403b729d1e
Gleave <adam.gleave@spirent.com>
2020-11-24 17:58:14 +0000

    Add initial benchmarking

---
+++ benches/benchmarks.rs
@@ -0,0 +1,16 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use tes_parse::{read_plugin};
+use std::fs::File;
+use std::path::PathBuf;
+
+fn read_benchmark(c: &mut Criterion) {
+    let path = PathBuf::from(format!("{}{}", env!("CARGO_MANIFEST_DIR"), "/data/Skyrim.esm"));
+
+    c.bench_function("read_plugin", move |b| b.iter_with_setup(
+        || File::open(&path).unwrap(),
+|file| read_plugin(file)
+    ));
+}
+
+criterion_group!(benches, read_benchmark);
+criterion_main!(benches);
